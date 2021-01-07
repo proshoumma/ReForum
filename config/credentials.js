594d62859a52619afc5354824569339d7b5b5831
Jiaxun Li <jiaxun1218@berkeley.edu>
2021-01-07 11:44:50 -0800

    resolved conflicts

--- config/credentials.js
+++ config/credentials.js
@@ -1,11 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
+
   GITHUB_CLIENT_ID: '4c8ea8f609f015fb072f',
   GITHUB_CLIENT_SECRET: '4179b59e8e99944312c1ac45b7de8b1308f2f5e3',
-=======
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
->>>>>>> 6b9f4ec639933a681641350b2485b2e4447dced3
   GITHUB_CALLBACK_URL: 'http://localhost:8080/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://127.0.0.1:27017',
 };
