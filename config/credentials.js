51ddd201eb8d3778a589a8f56c61128edba29f0a
Yueheng Zhang <azicon@berkeley.edu>
2021-01-13 22:21:22 -0800

    deleted validation

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
+  GITHUB_CLIENT_ID: 'd52ee29c2e854e1d4cd6',
+  GITHUB_CLIENT_SECRET: '072468116e48b54565a3aea7bce6263fe01f131a',
   GITHUB_CALLBACK_URL: 'http://localhost:8080/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://127.0.0.1:27017',
 };
